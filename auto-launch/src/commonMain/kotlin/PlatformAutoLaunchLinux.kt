63a278ca0cb1f8607f7dac41c09b6bf2aedb2bc3
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-02-23 14:02:19 +0100

    🚧 Work in progress for Linux

---
+++ auto-launch/src/commonMain/kotlin/PlatformAutoLaunchLinux.kt
@@ -0,0 +1,13 @@
+internal class PlatformAutoLaunchLinux(private val config: AutoLaunchConfig) : PlatformAutoLaunch {
+    override suspend fun isEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun enable() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun disable() {
+        TODO("Not yet implemented")
+    }
+}
